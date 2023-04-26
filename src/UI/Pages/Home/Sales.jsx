20d1ca4318c0f332b30aea459bbf98fc042ffc11
Racksoup <rack.connor1@gmail.com>
2023-04-26 17:57:20 -0400

    sales items update

--- src/UI/Pages/Home/Sales.jsx
+++ src/UI/Pages/Home/Sales.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Sales.scss';
-import Eagle from '../../../Assets/images/Eagle.png';
+import Eagle from '../../../Assets/images/ColorMan.jpg';
 
 const Sales = () => {
   return (
@@ -13,11 +13,13 @@ const Sales = () => {
             <Item />
             <Item />
             <Item />
+            <Item />
           </div>
         </div>
         <div className='Box' style={{ marginTop: '19rem' }}>
           <h3>Largest Sales</h3>
           <div className='Inner'>
+            <Item />
             <Item />
             <Item />
             <Item />
@@ -33,13 +35,21 @@ const Item = () => {
   return (
     <div className='Item'>
       <h5>genesis</h5>
+      <div className='Gradient' />
       <img src={Eagle} alt='NFT Art' />
+      <div className='InfoLine' style={{ marginTop: '.8rem' }}>
+        <p>Seller</p>
+        <p>Georgieo</p>
+      </div>
+      <div className='InfoLine'>
+        <p>Buyer</p>
+        <p>Martino</p>
+      </div>
+      <div className='Divider'></div>
       <div className='PriceLine'>
         <p className='Dollar'>$</p>
         <p className='Price'>400</p>
       </div>
-      <p>Seller: Genesis</p>
-      <p>Buyer: Genesis</p>
     </div>
   );
 };
