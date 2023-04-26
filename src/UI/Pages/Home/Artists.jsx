987cd95a40e2d4859b67885554ccd78a79f07eab
Racksoup <rack.connor1@gmail.com>
2023-04-26 18:20:13 -0400

    artists created

--- src/UI/Pages/Home/Artists.jsx
+++ src/UI/Pages/Home/Artists.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './Artists.scss';
+import ColorMan from '../../../Assets/images/ColorMan.jpg';
 
 const Artists = () => {
   return (
     <div className='Artists'>
       <div className='Content'>
-        <button className='BrowseBtn'>browse nft's</button>.
+        <div className='TopLine'>
+          <h4>our artists</h4>
+          <button className='BrowseBtn'>browse nft's</button>
+        </div>
+        <div className='Bottom'>
+          <Item />
+          <Item />
+          <Item />
+        </div>
       </div>
+    </div>
+  );
+};
+
+const Item = () => {
+  return (
+    <div className='Item'>
+      <img src={ColorMan} alt='Artist' />
+      <h5>Jamie</h5>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Vitae nunc sed velit dignissim sodales ut eu. Morbi tincidunt
+        augue interdum velit euismod.
+      </p>
     </div>
   );
 };
