ffabe310f174eb71882ae64b055664b157ab1335
Racksoup <rack.connor1@gmail.com>
2023-04-26 16:16:28 -0400

    changed app main css and structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.scss';
 import Home from './UI/Pages/Home/Home.jsx';
+import { store } from './Redux/store';
 
-import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='App-Main'>
-      <div className='App-Background' />
-      <div className='App-Content'>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-        </Routes>
+    <Router>
+      <div className='App'>
+        <Provider store={store}>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+          </Routes>
+        </Provider>
       </div>
-    </div>
+    </Router>
   );
 };
 
